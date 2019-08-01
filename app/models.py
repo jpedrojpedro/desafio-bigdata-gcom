c21cfcef866cf7ef94cf6a695669981965bbcb8a
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2019-08-01 18:54:57 -0300

    Implementando rotas com seus tipos corretos
    - Definindo corretamente se é GET, POST ou DELETE
    - Retornando os resultados como JSON
    - Utilizando métodos no Model para interagir com o BD

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import datetime
 import peewee as pw
 from app import db
+from playhouse.shortcuts import model_to_dict
 
 
 class BaseModel(pw.Model):
@@ -13,6 +14,39 @@ class VideoHistory(BaseModel):
     user = pw.CharField()
     url = pw.CharField()
     dt_viewed = pw.DateTimeField(default=datetime.datetime.now)
+
+    def clean_url(self):
+        self.url = self.url[:self.url.find('?')]
+
+    def to_dict(self):
+        vh_dict = model_to_dict(self)
+        vh_dict['dt_viewed'] = str(vh_dict['dt_viewed'])
+        return vh_dict
+
+    # TODO: implementar metodo similar
+    @classmethod
+    def similar(cls):
+        examples = [
+            {
+                "url": "g1.globo.com/educacao/enem/noticia/2019/04/02/inep-diz-que-enem-2019-nao-sofrera-alteracao-apos-grafica-declarar-falencia.ghtml",
+                "score": 0.9
+            }, {
+                "url": "www.globoplay.com/v/12758494",
+                "score": 0.85
+            }, {
+                "url": "g1.globo.com/pop-arte/musica/blog/mauro-ferreira/post/2019/04/02/los-hermanos-experimenta-leveza-na-primeira-musica-inedita-da-banda-em-14-anos.ghtml",
+                "score": 0.8
+            }, {
+                "url": "globoesporte.globo.com/blogs/blog-do-rodrigo-capelo/post/2019/04/02/o-flamengo-nao-investiu-mais-de-r-100-milhoes-em-jogadores-por-acaso-eis-os-numeros-de-2018.ghtml",
+                "score": 0.5
+            }
+        ]
+        return examples
+
+    @classmethod
+    def delete_all(cls):
+        cls.truncate_table(restart_identity=True)
+        return {}
 
     class Meta:
         table_name = 'video_history'
--- app/views.py
+++ app/views.py
@@ -1,19 +1,36 @@
 from app import app
+from models import VideoHistory
+from flask import request, abort, jsonify
 
-# TODO: deve ser POST
-@app.route("/<path:url>/view", methods=['GET'])
-@app.route("/<path:url>/view/", methods=['GET'])
+
+@app.route("/<path:url>/view/", methods=['POST'])
 def view(url):
-    return "View: {}".format(url)
+    user = request.form.get('user')
+    __video_history_params(url, user)
+    vh = VideoHistory(user=user, url=url)
+    vh.clean_url()
+    vh.save()
+    return jsonify(vh.to_dict())
 
 
-@app.route("/<path:url>/similar", methods=['GET'])
 @app.route("/<path:url>/similar/", methods=['GET'])
 def similar(url):
-    return "Similar: {}".format(url)
+    __url_params(url)
+    return jsonify(VideoHistory.similar())
 
 
-# TODO: deve ser DELETE
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['DELETE'])
 def delete():
-    return "Delete!"
+    return jsonify(VideoHistory.delete_all())
+
+
+def __video_history_params(url, user):
+    if url is None or user is None:
+        abort(404)
+    if len(url) == 0 or len(user) < 3:
+        abort(404)
+
+
+def __url_params(url):
+    if url is None or len(url) == 0:
+        abort(404)
